0b5267f5a3962d497421031984942f955ed99f2f
SuBoXian <68361150+SuBoXian@users.noreply.github.com>
2020-12-16 11:23:51 +0800

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,15 +1,16 @@
 `include "def.v"
 //`define INTERNAL_BITS 32
 //`define DATA_BITS 32
-module Adder(
+module ALU(
 	Data_in1,
 	Data_in2,
+	Op,
+	Zero,
 	Result
 );
 
 	input [`INTERNAL_BITS-1:0] Data_in1,Data_in2;
-	output [`INTERNAL_BITS-1:0] Result;
-
- assign Result = Data_in1 + Data_in2;
+	input [`ALU_OP-1:0] Op;
+	output [`INTERNAL_BITS-1:0] Result,Zero;
 
 endmodule
