19f083723a1c56e6a7d5855304fe16cb3e4843da
SuBoXian <68361150+SuBoXian@users.noreply.github.com>
2020-12-22 01:17:04 +0800

    Add files via upload

--- sim/Registers/Registers.v
+++ sim/Registers/Registers.v
@@ -1,15 +1,42 @@
 `include "def.v"
-//`define INTERNAL_BITS 32
-//`define DATA_BITS 32
-module Adder(
-	Data_in1,
-	Data_in2,
-	Result
+
+module Register(
+	CLKA,
+	Read_reg1,
+	Read_reg2,
+	Read_data1,
+	Read_data2,
+	CLKB,
+	Write_enable,
+	Write_reg,
+	Write_data
 );
 
-	input [`INTERNAL_BITS-1:0] Data_in1,Data_in2;
-	output [`INTERNAL_BITS-1:0] Result;
+	//parameters
+	parameter ADDR_BITS = 5;
+	parameter REG_SIZE = 32;
 
- assign Result = Data_in1 + Data_in2;
+	input CLKA,CLKB;
+	input Write_enable;
+	input [ADDR_BITS-1:0] Read_reg1,Read_reg2,Write_reg;
+	input [`INTERNAL_BITS-1:0] Write_data;
+	output [`INTERNAL_BITS-1:0] Read_data1,Read_data2;
+
+	reg [`INTERNAL_BITS-1:0] REG [0:REG_SIZE-1];
+
+	//Port A read only
+	always@(posedge CLKA)
+	begin
+		Read_data1 <= REG[Read_reg1];
+		Read_data2 <= REG[Read_reg2];
+	end
+
+	//Port B write only
+	always@(posedge CLKB)
+	begin
+		if(Write_enable)
+			REG[Write_reg] <= Write_data;
+		else;
+	end
 
 endmodule
