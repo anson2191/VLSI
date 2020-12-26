6efcc8ae1c82f66b54506b66d1951b3e8b6ee33b
anson2191 <76254909+anson2191@users.noreply.github.com>
2020-12-26 16:07:56 +0800

    Update Adder.v

--- Adder.v
+++ Adder.v
@@ -1,15 +1,12 @@
 `include "def.v"
-//`define INTERNAL_BITS 32
-//`define DATA_BITS 32
 module Adder(
-	Data_in1,
-	Data_in2,
+	in,
 	Result
 );
 
-	input [`INTERNAL_BITS-1:0] Data_in1,Data_in2;
-	output [`INTERNAL_BITS-1:0] Result;
+	input [`PC_BITS-1:0] in;
+	output [`PC_BITS-1:0] Result;
 
- assign Result = Data_in1 + Data_in2;
+ assign out = in + 32'd4;
 
 endmodule
