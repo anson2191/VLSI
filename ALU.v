1c410af55fca2001e899cf73780a16296d7001f1
anson2191 <76254909+anson2191@users.noreply.github.com>
2020-12-26 16:41:04 +0800

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -1,17 +1,24 @@
 `include "def.v"
-//`define INTERNAL_BITS 32
 //`define DATA_BITS 32
+//`define INTERNAL_BITS 16
+//`define ALUOP_BITS 4
+
 module ALU(
 	Data_in1,
 	Data_in2,
-	Op,
+	ALUop,
 	Zero,
 	Result
 );
 
 	input [`INTERNAL_BITS-1:0] Data_in1,Data_in2;
-	input [`ALU_OP-1:0] Op;
-	output [`INTERNAL_BITS-1:0] Result,Zero;
-
-	assign Result = 32'bz;
+	input [`ALUOP_BITS-1:0] ALUop;
+	output [`INTERNAL_BITS-1:0] Result, Zero;
+	
+	always@(*) begin
+		case(ALUop)
+			
+		endcase
+	end
+	
 endmodule
