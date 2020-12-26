77ec9437de7e7c5e574cab0c85d8ab23cd5f7839
anson2191 <76254909+anson2191@users.noreply.github.com>
2020-12-27 00:56:40 +0800

    Update ALU.v

--- ALU.v
+++ ALU.v
@@ -10,14 +10,46 @@ module ALU(
 	Zero,
 	Result
 );
-
+	parameter AND = 4'b0000,
+			  OR  = 4'b0001,
+			  ADD = 4'b0010,
+			  SUB = 4'b0110,
+			  SLT = 4'b0111,
+			  NOR = 4'b1100;
+	
 	input [`INTERNAL_BITS-1:0] Data_in1,Data_in2;
 	input [`ALUOP_BITS-1:0] ALUop;
 	output [`INTERNAL_BITS-1:0] Result, Zero;
 	
 	always@(*) begin
 		case(ALUop)
+			AND:begin
+				Result = Data_in1&Data_in2;//bit-wise
+			end
 			
+			OR:begin
+				Result = Data_in1|Data_in2;//bit-wise
+			end
+			
+			ADD:begin //CLA?
+				Result = Data_in1 + Data_in2;
+			end
+			
+			SUB:begin
+				
+			end
+			
+			SLT:begin
+			
+			end
+			
+			NOR:begin
+			
+			end
+			
+			default:begin
+			
+			end			
 		endcase
 	end
 	
