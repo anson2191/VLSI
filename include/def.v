a83c5646990f6cc487a4646f0d983ae712746bc8
anson2191 <76254909+anson2191@users.noreply.github.com>
2021-01-11 15:14:11 +0800

    Update def.v

--- include/def.v
+++ include/def.v
@@ -6,7 +6,12 @@
 `define INTERNAL_BITS 16 //?
 
 `define PC_BITS 32
-`define ALUOP_BITS 4
+`define OPCODE_BITS 6
+`define opcode ir[31:26]
+`define FUNCTION_BITS 6
+`defin `function ir[5:0]
+`define ALUOP_BITS 2
+`define ALUCONTROL_BITS 4
 
 
 `endif
