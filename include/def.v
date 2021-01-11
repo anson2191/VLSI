0cd6c8bb07c6fc7febd554ac161f8e5708410de4
anson2191 <76254909+anson2191@users.noreply.github.com>
2021-01-11 15:35:58 +0800

    Update def.v

--- include/def.v
+++ include/def.v
@@ -1,15 +1,34 @@
 `ifndef DEF_V
 `define DEF_V
 
-//Global
-`define DATA_BITS 32
-`define INTERNAL_BITS 16 //?
+`define INTERNAL_BITS 32
+//----------------------instruction field------------------------
+`define OPCODE 31:26
+//RTYPE
+`define RTYPE_RS 25:21
+`define RTYPE_RT 20:16
+`define RTYPE_RD 15:11
+`define RTYPE_SHAMT 10:6
+`define RTYPE_FUNC 5:0
+//JTYPE
+`define JTYPE_CONST 25:0
+//ITYPE
+`define ITYPE_RS 25:21
+`define ITYPE_RT 20:16
+`define ITYPE_IMM 15:0
+//-------------------------OPCODE---------------------------------
+`define RTYPE 6'b000_000
+`define LW 6'b100_011
+`define SW 6'b101_011
+`define BEQ 6'b000_100
+`define JMP 6'b000_010
+//----------------------Special instruction-----------------------
+`define NOP 32'h0000_0000
+`define SYSCALL 32'h0000_000c
 
 `define PC_BITS 32
 `define OPCODE_BITS 6
-`define opcode ir[31:26]
 `define FUNCTIONCODE_BITS 6
-`define functioncode ir[5:0]
 `define ALUOP_BITS 2
 `define ALUCONTROL_BITS 4
 
